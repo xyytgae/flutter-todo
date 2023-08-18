d6c36a3fd490925d2745d86c0983b6cfd3b62b3a
xyytgae <xyytgae@yahoo.co.jp>
2023-08-19 01:59:00 +0900

    feat: リスト追加画面作成

--- lib/main.dart
+++ lib/main.dart
@@ -25,9 +25,35 @@ class TodoListPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
+    return Scaffold(
+      body: const Center(
         child: Text('リスト一覧画面'),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.of(context).push(
+            MaterialPageRoute(builder: (context) {
+              return TodoAddPage();
+            }),
+          );
+        },
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
+
+class TodoAddPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: TextButton(
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+          child: const Text('リスト追加画面（クリックで戻る）'),
+        ),
       ),
     );
   }
